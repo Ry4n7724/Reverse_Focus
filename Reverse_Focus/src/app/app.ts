986f82fd978f2f09ddbdf3b8087b3ae2f56081c8
retro <ryantrut@proton.me>
2026-01-29 21:23:24 +0100

    Created Navbar
    - Created navbar component
    - Implemented darkmode

--- Reverse_Focus/src/app/app.ts
+++ Reverse_Focus/src/app/app.ts
@@ -1,13 +1,16 @@
 import { TuiRoot } from "@taiga-ui/core";
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./navbar/navbar";
+import { TUI_DARK_MODE, TUI_DARK_MODE_KEY } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('Reverse_Focus');
+  protected readonly darkMode = inject(TUI_DARK_MODE);
 }
