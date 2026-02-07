c1177e3831cb00402740dd3a32a91eca0da53623
retro <ryantrut@proton.me>
2026-02-07 18:57:35 +0100

    Added session page

--- Reverse_Focus/src/app/app.routes.ts
+++ Reverse_Focus/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { StarterPage } from './starter-page/starter-page';
+import { CreateSessionPage } from './create-session-page/create-session-page';
 
-export const routes: Routes = [{
-    path: '', component: StarterPage
-}];
+export const routes: Routes = [
+    { path: '', component: StarterPage },
+    { path: 'create-session', component: CreateSessionPage }
+];
--- Reverse_Focus/src/app/bottom-navigation/bottom-navigation.ts
+++ Reverse_Focus/src/app/bottom-navigation/bottom-navigation.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TuiIcon } from "@taiga-ui/core";
 
 @Component({
   selector: 'app-bottom-navigation',
-  imports: [TuiIcon],
+  imports: [TuiIcon, RouterLink],
   templateUrl: './bottom-navigation.html',
   styleUrl: './bottom-navigation.css',
 })
