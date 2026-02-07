7508d84528d1a14ef26600d0aaf4019949b6ca70
Ry4n7724 <RyanTrut@proton.me>
2026-02-07 09:43:29 +0100

    Added bottom navbar component.

---
+++ Reverse_Focus/src/app/bottom-navigation/bottom-navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-navigation',
+  imports: [],
+  templateUrl: './bottom-navigation.html',
+  styleUrl: './bottom-navigation.css',
+})
+export class BottomNavigation {
+
+}
