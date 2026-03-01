faaa105515281a0848d5749f8b8c4e25eedda8b6
retro <ryantrut@proton.me>
2026-03-01 10:56:05 +0100

    Fixed background service

--- Reverse_Focus/src/background.ts
+++ Reverse_Focus/src/background.ts
@@ -1,6 +1,6 @@
-import { db } from "./shared/session-db.js";
+import { db } from "./shared/session-db";
 
-chrome.webNavigation.onCompleted.addListener(async (details) => {
+chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
     if (details.frameId !== 0) return;
 
     const sessions = await db.sessionDB.toArray();
@@ -8,9 +8,10 @@ chrome.webNavigation.onCompleted.addListener(async (details) => {
     for (const session of sessions) {
         if (
             session.active &&
-            session.urls.some(url => details.url.includes(url))
+            !session.urls.some(url => details.url.includes(url))
+            && details.url !== "chrome://newtab/"
         ) {
-            await chrome.tabs.update(details.tabId, {
+            chrome.tabs.update(details.tabId, {
                 url: chrome.runtime.getURL("index.html#blocker")
             });
             return;
