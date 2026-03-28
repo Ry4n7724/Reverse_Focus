7864886ae202e13c1012680a1225ab7fa1cd153f
retro <ryantrut@proton.me>
2026-03-28 23:07:19 +0100

    Added auto save

--- Reverse_Focus/src/app/app.ts
+++ Reverse_Focus/src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { TUI_DARK_MODE, TuiRoot } from "@taiga-ui/core";
 import { BottomNavigation } from "./bottom-navigation/bottom-navigation";
 import { Navbar } from "./navbar/navbar";
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,33 @@ import { Navbar } from "./navbar/navbar";
   styleUrl: './app.css'
 })
 
-export class App {
+export class App implements OnInit {
   protected readonly darkMode = inject(TUI_DARK_MODE);
+  private readonly STORAGE_KEY = 'currentRoute';
+
   constructor(private router: Router) { }
 
   ngOnInit() {
     const params = new URLSearchParams(window.location.search);
     const route = params.get('route');
+    const currentHash = window.location.hash.replace('#', '');
 
     if (route) {
       this.router.navigateByUrl(route);
+      localStorage.setItem(this.STORAGE_KEY, route);
+    } else if (currentHash && currentHash !== '/') {
+      this.router.navigateByUrl(currentHash);
+    } else {
+      const savedRoute = localStorage.getItem(this.STORAGE_KEY);
+      if (savedRoute) {
+        this.router.navigateByUrl(savedRoute);
+      }
     }
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      localStorage.setItem(this.STORAGE_KEY, event.urlAfterRedirects);
+    });
   }
 }
--- Reverse_Focus/src/app/create-session-page/create-session-page.ts
+++ Reverse_Focus/src/app/create-session-page/create-session-page.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit, signal, effect, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, signal, effect, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
 import { TemplateForm } from "../template-form/template-form";
 import { FieldConfig } from '../template-form/field-config';
 import { FormGroup, Validators } from '@angular/forms';
 import { SessionService } from '../session-database/session-service';
 import { SessionDropdown } from "../session-dropdown/session-dropdown";
 import Session from '../session-database/session';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-session-page',
@@ -12,7 +13,7 @@ import Session from '../session-database/session';
   templateUrl: './create-session-page.html',
   styleUrl: './create-session-page.css',
 })
-export class CreateSessionPage implements OnInit {
+export class CreateSessionPage implements OnInit, AfterViewInit, OnDestroy {
   mode: Mode = Mode.CREATE;
   sessionService = inject(SessionService)
   formTitle = 'Create Session';
@@ -27,9 +28,48 @@ export class CreateSessionPage implements OnInit {
   sessions = signal<Session[]>([]);
   @ViewChild(TemplateForm) templateForm?: TemplateForm;
   editingSessionId?: number;
+  private readonly STORAGE_KEY = 'sessionFormState';
+  private formSubscription?: Subscription;
+
+  private saveToStorage(form: FormGroup) {
+    const state = {
+      sessionName: form.value.sessionName || '',
+      icon: form.value.icon || 'book',
+      urlList: form.value.urlList || []
+    };
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(state));
+  }
+
+  private clearStorage() {
+    localStorage.removeItem(this.STORAGE_KEY);
+  }
+
+  private loadFromStorage(): { sessionName: string; icon: string; urlList: string[] } | null {
+    const stored = localStorage.getItem(this.STORAGE_KEY);
+    return stored ? JSON.parse(stored) : null;
+  }
+
+  private setupFormSubscription(form: FormGroup) {
+    this.formSubscription?.unsubscribe();
+    this.formSubscription = form.valueChanges.subscribe(() => {
+      if (this.mode === Mode.CREATE) {
+        this.saveToStorage(form);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.formSubscription?.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    if (this.templateForm) {
+      this.setupFormSubscription(this.templateForm.form);
+    }
+  }
+
   constructor() {
     effect(() => {
-      console.log('Fields changed:', this.fields());
       const currentFields = this.fields();
       if (this.templateForm && this.mode === Mode.EDIT) {
         this.templateForm.form.patchValue({
@@ -38,8 +78,6 @@ export class CreateSessionPage implements OnInit {
           urlList: currentFields[1]?.value
         }, { emitEvent: false });
         this.templateForm.urlInputControl.clearValidators();
-      } else {
-        this.templateForm?.form.reset();
       }
     });
   }
@@ -55,16 +93,29 @@ export class CreateSessionPage implements OnInit {
       this.formTitle = 'Create Session';
       this.fields.set(this.defaultFields);
       this.loadSessions();
+      this.clearStorage();
     } else if (form.valid) {
       console.log(form.value)
       this.sessionService.addSession(form.value.sessionName, form.value.icon, form.value.urlList, false)
       form.reset()
       this.loadSessions();
+      this.clearStorage();
     }
   }
 
   async ngOnInit() {
     this.loadSessions();
+    const saved = this.loadFromStorage();
+    if (saved && this.mode === Mode.CREATE) {
+      this.fields.set([
+        {
+          name: 'sessionName', label: 'Session Name', type: 'inputWithIconPicker', validator: Validators.required,
+          value: saved.sessionName,
+          subInputs: [{ name: 'icon', label: 'Session Icon', type: 'icon-picker', value: saved.icon }]
+        },
+        { name: 'urlList', label: 'Add URLs', type: 'urlsInput', value: saved.urlList, placeholder: 'url1,url2,...' },
+      ]);
+    }
   }
 
   async loadSessions() {
@@ -75,6 +126,7 @@ export class CreateSessionPage implements OnInit {
     this.mode = Mode.EDIT;
     this.editingSessionId = $event.id;
     this.formTitle = 'Edit Session';
+    this.clearStorage();
     this.fields.set([
       {
         name: 'sessionName', label: 'Session Name', type: 'inputWithIconPicker', validator: Validators.required,
